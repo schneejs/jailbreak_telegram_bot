50fb875008cc6f2e7ecfd734297a90bc49beb6bd
ArcSineSoftware <>
2020-05-12 23:43:38 +0600

    1.0 version

---
+++ index.js
@@ -0,0 +1,38 @@
+const TelegramBot = require("node-telegram-bot-api");
+// Version support database
+const versup = require("./version_support");
+// Simple logging
+const info = (...msgs) => msgs.forEach(msg => console.log(msg));
+const error = (...msgs) => msgs.forEach(msg => console.error(msg));
+// Main configuration variables
+try {
+    require("dotenv").config();
+    info("Dotenv loaded");
+} catch (_) {
+    info("Dotenv not found, avoiding");
+}
+const token = process.env.TOKEN;
+// Main bot instance
+const bot = new TelegramBot(token, { polling: true });
+// IOS version mentioned
+bot.onText(/(1\d\.\d\.?\d?)/, (msg, match) => {
+    const chatId = msg.chat.id;
+    const full = match[0];
+    const examinateVersion = full => {
+        for (const version of versup) {
+            if (full >= version.from && full <= version.to) {
+                switch (version.state) {
+                case "full":
+                    return `Yes, your IOS version ${full} is fully supported!`
+                case "a11":
+                    return `Your IOS version ${full} is supported on iPhone X and lower. XR and 11 aren't supported.`;
+                }
+            }
+        }
+        return "Unfortunately, your IOS version is not supported.";
+    }
+    const resultText =
+        examinateVersion(full)
+        + " Trusted jailbreaks today are checkra.in and unc0ver.dev, other sites can be fake!";
+    bot.sendMessage(chatId, resultText);
+});
---
+++ version_support.js
@@ -0,0 +1,4 @@
+module.exports = [
+    {"from": "0", "to": "13.3", "state": "full"},
+    {"from": "12.3", "to": "99.9.9", "state": "a11"}
+];
