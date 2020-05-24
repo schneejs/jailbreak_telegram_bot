44d21a2283b6e9e7187c1a382e15bd7d8f75c5f1
ArcSineSoftware <>
2020-05-24 11:53:18 +0600

    Update supported version

--- version_support.js
+++ version_support.js
@@ -1,4 +1,4 @@
 module.exports = [
-    {"from": "0", "to": "13.3", "state": "full"},
-    {"from": "12.3", "to": "99.9.9", "state": "a11"}
+    {"from": "0", "to": "13.5", "state": "full"},
+    {"from": "13.5.1", "to": "99.9.9", "state": "a11"}
 ];
