739796ad426e4cb67c6c27e9c4ae9b1e9276ba2d
ArcSineSoftware <>
2020-05-27 10:18:20 +0600

    Fix supported versions bug

--- version_support.js
+++ version_support.js
@@ -1,4 +1,5 @@
 module.exports = [
-    {"from": "0", "to": "13.5", "state": "full"},
-    {"from": "13.5.1", "to": "99.9.9", "state": "a11"}
+    {"from": "0", "to": "12.4.6", "state": "full"},
+    {"from": "12.4.7", "to": "12.9.9", "state": "a11"},
+    {"from": "13.0", "to": "13.5", "state": "full"},
 ];
