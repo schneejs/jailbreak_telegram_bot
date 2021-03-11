4f7a92a901efff053762896ef2c934866978bbf9
schneejs <13682559+schneejs@users.noreply.github.com>
2021-03-11 13:44:00 +0600

    update tools

--- tools.js
+++ tools.js
@@ -2,33 +2,26 @@ module.exports = [
     {
         name: 'checkra1n β',
         website: 'http://checkra.in',
-        versions: [{ from: '14.0' }],
-        note: 'only iPhone 6, 6S and SE (1st gen) are supported, Mac or Linux is required.',
-    },
-    {
-        name: 'checkra1n β',
-        website: 'http://checkra.in',
-        versions: [{ from: '12.0', to: '13.7' }],
-        note: 'only iPhone 6, 6S, 7, 8 and X are supported, XS and 11 are not, Mac or Linux is required.',
+        versions: [{ from: '12.0' }],
+        note: 'only iPhone 6, 6S, 7, 8 and X are supported, XS and 11 are not, Mac or Linux is required',
     },
     {
         name: 'Odyssey β',
         website: 'https://theodyssey.dev/',
-        versions: [{ from: '13.0', to: '13.5' }],
+        versions: [{ from: '13.0', to: '13.7' }],
     },
     {
         name: 'Unc0ver',
         website: 'https://unc0ver.dev/',
-        versions: [{ from: '11.0', to: '13.5' }],
+        versions: [{ from: '11.0', to: '14.3' }],
     },
     {
         name: 'Chimera',
         website: 'http://chimera.sh',
         versions: [
-            { from: '12.0', to: '12.2' },
-            { from: '12.4', to: '12.4' }
+            { from: '12.0', to: '12.5.1' },
         ],
-        note: '12.1.3 - 12.3 and 12.4 only supported on A7 - A11 devices.'
+        note: '12.1.3 - 12.5.1 only supported on A7 - A11 devices. All devices supported on 12.0 - 12.1.2'
     },
     {
         name: 'Electra',
